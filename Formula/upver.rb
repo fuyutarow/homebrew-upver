3f4ced6a62de7377eaf12148e3319ec029ee8378
fuyutarow <fuyutarow@gmail.com>
2020-05-07 14:03:10 +0900

    Up: 0.20200506.0

--- Formula/upver.rb
+++ Formula/upver.rb
@@ -1,8 +1,7 @@
 class Upver < Formula
   desc "A manager for SemVer"
   homepage "https://github.com/fuyutarow/UpVER"
-  url "https://github.com/fuyutarow/UpVER/archive/v0.202003.6.tar.gz"
-  sha256 "88b58100868900d139d3325ca6b8731ac307e472a40e4fb1fe160ecf9cc1a1ef"
+  url "https://github.com/fuyutarow/UpVER/archive/v0.202005.0.tar.gz"
 
   def install
     bin.install "bin/upver"
