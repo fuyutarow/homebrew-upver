aaec4de1ed08a635518e4678dc986dd531b9c0ea
fuyutarow <fuyutarow@gmail.com>
2020-05-12 05:55:32 +0900

    Up

--- Formula/upver.rb
+++ Formula/upver.rb
@@ -1,10 +1,15 @@
 class Upver < Formula
   desc "A manager for SemVer"
   homepage "https://github.com/fuyutarow/UpVER"
-  url "https://github.com/fuyutarow/UpVER/archive/v0.202005.0.tar.gz"
+  # url "https://github.com/fuyutarow/UpVER/archive/v0.202005.0.tar.gz"
+  head "https://github.com/fuyutarow/UpVER.git"
+
+  depends_on "rust" => :build
 
   def install
-    bin.install "bin/upver"
+    system "cargo", "build", "--release"
+
+    bin.install "target/release/upver"
   end
 end
 
