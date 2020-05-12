5f260ca754b04b8d9f2a27b898bcc6bde3593eb2
fuyutarow <fuyutarow@gmail.com>
2020-05-12 14:56:50 +0900

    Up

--- Formula/upver.rb
+++ Formula/upver.rb
@@ -1,13 +1,16 @@
 class Upver < Formula
   desc "A manager for SemVer"
   homepage "https://github.com/fuyutarow/UpVER"
-  # url "https://github.com/fuyutarow/UpVER/archive/v0.202005.0.tar.gz"
   head "https://github.com/fuyutarow/UpVER.git"
+
+  bottle do
+    cellar :any_skip_relocation
+  end
 
   depends_on "rust" => :build
 
   def install
-    system "make", "install", "PREFIX=#{prefix}"
+    system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 end
 
